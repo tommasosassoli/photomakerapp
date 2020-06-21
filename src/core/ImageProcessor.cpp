8627763adc7f4bedae2f6212655fe96ef6624511
tommasosassoli <tommaso.sassoli@stud.unifi.it>
2020-06-21 18:06:34 +0200

    Crop fixed

--- src/core/ImageProcessor.cpp
+++ src/core/ImageProcessor.cpp
@@ -122,7 +122,7 @@ namespace ImageProcessor {
     Image<T> crop(const Image<T> &img, int x1, int y1, int x2, int y2) {
         if ((x2 > x1) && (y2 > y1) &&
             (x1 >= 0) && (y1 >= 0) &&
-            (x2 < img.getHeight()) && (y2 < img.getWidth())) {
+            (x2 <= img.getHeight()) && (y2 <= img.getWidth())) {
             int height = x2 - x1;
             int width = y2 - y1;
             Image<T> newimg(width, height);
