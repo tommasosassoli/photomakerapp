68aa47bb0a7ce5a2de3795a33f91d4bc8c78fcd1
tommasosassoli <tommaso.sassoli@stud.unifi.it>
2020-07-06 16:35:28 +0200

    Test undo/redo: check image change

--- test/TestUndoRedo.cpp
+++ test/TestUndoRedo.cpp
@@ -8,51 +8,85 @@
 #include "../src/command/CommandHandler.cpp"
 #include "../src/command/FlipCommand.h"
 #include "../src/command/MirrorCommand.h"
+#include "../src/view/ImageWrapper.h"
+#include "../src/view/ImageWrapper.cpp"
 
-class TestUndoRedo : public ::testing::Test {
+class TestUndoRedo : public ::testing::Test, public Observer {
+public:
+    void update() override {
+        testImg = imgWrapper->getImage();
+    }
+
 protected:
     virtual void SetUp() {
         std::ifstream imgFile("../../test/testImage/convolution/Vd-Orig.ppm");
         if(imgFile.is_open()) {
             Image<> img;
             imgFile >> img;
-            testImg = std::make_shared<Image<>>(img);
+            origTestImg = std::make_shared<Image<>>(img);
+
+            imgWrapper = new ImageWrapper();
+            imgWrapper->addObserver(this);
         } else
             GTEST_FATAL_FAILURE_("Cannot load the test image");
     }
 
-    shared_ptr<Image<>> testImg; //TODO controllo dell'immagine
+    virtual void TearDown() {
+        if(imgWrapper != nullptr) {
+            imgWrapper->removeObserver(this);
+            delete imgWrapper;
+        }
+    }
+
+    shared_ptr<Image<>> origTestImg;
+    shared_ptr<Image<>> testImg;
     CommandHandler cmdHandler;
+    ImageWrapper* imgWrapper {nullptr};
 };
 
-TEST_F(TestUndoRedo, RegisterCmd) {
-    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(testImg);
-    cmdHandler.registerAndExecute(flipCmd);
 
-    shared_ptr<MirrorCommand> mirrorCmd = std::make_shared<MirrorCommand>(testImg);
+TEST_F(TestUndoRedo, RegisterCmd) {
+    testImg.reset();
+
+    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(origTestImg);
+    cmdHandler.registerAndExecute(flipCmd);
+    imgWrapper->setImage(flipCmd->getParsedImage());
+
+    shared_ptr<MirrorCommand> mirrorCmd = std::make_shared<MirrorCommand>(origTestImg);
     cmdHandler.registerAndExecute(mirrorCmd);
+    imgWrapper->setImage(mirrorCmd->getParsedImage());
 
     // get top of undo
     Command *cmd = cmdHandler.getUndoTop().get();
     MirrorCommand *mirror = dynamic_cast<MirrorCommand*>(cmd);
     if(mirror == nullptr)
         GTEST_FAIL();
+
+    if(!(testImg == mirrorCmd->getParsedImage()))
+        GTEST_FAIL();
 }
 
 TEST_F(TestUndoRedo, UndoCmd) {
-    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(testImg);
+    testImg.reset();
+
+    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(origTestImg);
     cmdHandler.registerAndExecute(flipCmd);
+    imgWrapper->setImage(flipCmd->getParsedImage());
 
     if(cmdHandler.isUndoPossible()) {
-        cmdHandler.undo();
+        shared_ptr<Command> undoCmd = cmdHandler.undo();
+        imgWrapper->setImage(undoCmd->getPreviousImage());
 
         if(cmdHandler.isUndoPossible())
             GTEST_FAIL();
 
         // get top of redo
         Command *cmdr = cmdHandler.getRedoTop().get();
-        FlipCommand *mirror = dynamic_cast<FlipCommand*>(cmdr);
-        if(mirror == nullptr)
+        FlipCommand *flip = dynamic_cast<FlipCommand*>(cmdr);
+        if(flip == nullptr)
+            GTEST_FAIL();
+
+        if(!(testImg == undoCmd->getPreviousImage()))
             GTEST_FAIL();
 
     } else
@@ -60,12 +94,17 @@ TEST_F(TestUndoRedo, UndoCmd) {
 }
 
 TEST_F(TestUndoRedo, RedoCmd) {
-    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(testImg);
+    testImg.reset();
+
+    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(origTestImg);
     cmdHandler.registerAndExecute(flipCmd);
+    imgWrapper->setImage(flipCmd->getParsedImage());
     cmdHandler.undo();
+    imgWrapper->setImage(flipCmd->getPreviousImage());
 
     if(cmdHandler.isRedoPossible()) {
-        cmdHandler.redo();
+        shared_ptr<Command> redoCmd = cmdHandler.redo();
+        imgWrapper->setImage(redoCmd->getParsedImage());
 
         if(cmdHandler.isRedoPossible())
             GTEST_FAIL();
@@ -75,16 +114,26 @@ TEST_F(TestUndoRedo, RedoCmd) {
         FlipCommand *mirror = dynamic_cast<FlipCommand*>(cmd);
         if(mirror == nullptr)
             GTEST_FAIL();
+
+        if(!(testImg == redoCmd->getParsedImage()))
+            GTEST_FAIL();
+
     } else
         GTEST_FAIL();
 }
 
 TEST_F(TestUndoRedo, UndoAndRegister) {
-    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(testImg);
+    testImg.reset();
+
+    shared_ptr<FlipCommand> flipCmd = std::make_shared<FlipCommand>(origTestImg);
     cmdHandler.registerAndExecute(flipCmd);
-    shared_ptr<FlipCommand> flipCmd1 = std::make_shared<FlipCommand>(testImg);
+    imgWrapper->setImage(flipCmd->getParsedImage());
+    shared_ptr<FlipCommand> flipCmd1 = std::make_shared<FlipCommand>(origTestImg);
     cmdHandler.registerAndExecute(flipCmd1);
-    cmdHandler.undo();
+    imgWrapper->setImage(flipCmd1->getParsedImage());
+
+    shared_ptr<Command> undoCmd = cmdHandler.undo();
+    imgWrapper->setImage(undoCmd->getPreviousImage());
 
     if(cmdHandler.isRedoPossible()) {
         // get top of redo
@@ -93,8 +142,12 @@ TEST_F(TestUndoRedo, UndoAndRegister) {
         if(flip == nullptr)
             GTEST_FAIL();
 
-        shared_ptr<MirrorCommand> mirrorCmd = std::make_shared<MirrorCommand>(testImg);
+        if(!(testImg == undoCmd->getPreviousImage()))
+            GTEST_FAIL();
+
+        shared_ptr<MirrorCommand> mirrorCmd = std::make_shared<MirrorCommand>(origTestImg);
         cmdHandler.registerAndExecute(mirrorCmd);
+        imgWrapper->setImage(mirrorCmd->getParsedImage());
 
         // now the redo stack should be empty
         if(cmdHandler.isRedoPossible())
@@ -104,6 +157,9 @@ TEST_F(TestUndoRedo, UndoAndRegister) {
         Command *cmd1 = cmdHandler.getUndoTop().get();
         MirrorCommand *mirror = dynamic_cast<MirrorCommand*>(cmd1);
         if(mirror == nullptr)
+            GTEST_FAIL();
+
+        if(!(testImg == mirrorCmd->getParsedImage()))
             GTEST_FAIL();
 
     } else
